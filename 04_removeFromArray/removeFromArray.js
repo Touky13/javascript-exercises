087cd2ccd455206a9e8f61b393435b998488de3d
Touky13 <197660339+Touky13@users.noreply.github.com>
2025-02-19 18:07:04 +0100

    First array done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, spliceElem) {
+
+    const splicedArray = array.toSpliced(spliceElem - 1, 1);
+
+    return splicedArray;
+
 };
 
 // Do not edit below this line
