dd530294435b5c653525c9c877079b74cbde6ac3
Touky13 <197660339+Touky13@users.noreply.github.com>
2025-02-19 20:08:21 +0100

    Halfway done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
 
+    let result = 0;
+    for (let i = startInt; i <= endInt; i++) {
+        result += i
+    }
+    return result;
+      
 };
 
 // Do not edit below this line
