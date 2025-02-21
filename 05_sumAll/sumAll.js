93302304bd5673d5f924570c48668bbab1ca0e61
Touky13 <197660339+Touky13@users.noreply.github.com>
2025-02-21 11:19:31 +0100

    Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
 const sumAll = function(startInt, endInt) {
 
+    if (startInt > endInt){
+        let minTemp = Math.min(startInt, endInt);
+        let maxTemp = Math.max (startInt, endInt);
+        startInt = minTemp;
+        endInt = maxTemp;
+    } else if (startInt < 0 || !Number.isInteger(startInt) || endInt < 0 || !Number.isInteger(endInt)){
+        return "ERROR";
+    }
+
     let result = 0;
     for (let i = startInt; i <= endInt; i++) {
         result += i
