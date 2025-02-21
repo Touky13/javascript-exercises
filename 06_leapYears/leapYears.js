4e77ddabf5dc0e1ec0d716f1cf45fcc1d9924810
Touky13 <197660339+Touky13@users.noreply.github.com>
2025-02-21 11:36:07 +0100

    Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+     let leapYearTest = year % 4;
+     let leapYearTest100 = year % 100;
+     let leapYearTest400 = year % 400;
+    if (leapYearTest === 0 && leapYearTest100 !== 0 || leapYearTest400 ===0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
