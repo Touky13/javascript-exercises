7c71a38a75e6ae6efac662c08c4be40ce243702b
Touky13 <197660339+Touky13@users.noreply.github.com>
2025-02-21 12:06:17 +0100

    Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  const fahrenheitToCelsius = (fahrenheit - 32) * 5/9;
+
+  if (Number.isInteger(fahrenheitToCelsius)) {
+    return fahrenheitToCelsius;
+  } else {
+    return Number(fahrenheitToCelsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  const celsiusToFahrenheit = celsius * 9/5 +32;
+
+  if (Number.isInteger(celsiusToFahrenheit)) {
+  return celsiusToFahrenheit;
+  } else {
+    return Number(celsiusToFahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
